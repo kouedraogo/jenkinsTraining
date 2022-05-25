aabeb6adc75dfff8a819870d2ccef0f7ed8dbfff
OUEDRAOGO Abel <abel.ouedraogo@viveris.fr>
2022-05-25 12:02:13 +0200

    Adding project demo to my github

---
+++ src/main/java/com/example/jenkins/demo/JenkinsCalculator.java
@@ -0,0 +1,14 @@
+package com.example.jenkins.demo;
+
+public class JenkinsCalculator {
+	
+	//Method to add 2 numbers
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	//Method to subtract 2 numbers
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+}
---
+++ src/test/java/com/example/jenkins/demo/JenkinsCalcTest.java
@@ -0,0 +1,23 @@
+package com.example.jenkins.demo;
+
+import static org.junit.Assert.*;
+
+//import org.junit.Test;
+import org.testng.annotations.Test;
+
+
+public class JenkinsCalcTest {
+
+	@Test
+	public void addTest() {
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10, myCalc.addNumbers(5, 5));
+	}
+	
+	@Test
+	public void subtractTest() {
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5, myCalc.subtractNumbers(10, 5));
+	}
+
+}
